d0200b80a18bc93c10e94420ea72264cdd8d2175
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-15 12:27:18 -0800

    built out classes and interfaces; integrated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Zoo
 CF 401 - Lab: Zoo (OOP Principles)
+
+
+## Class Map
+Map of inheritance, and includes abstraction.
+![class map](assets/class-map.pdf)
--- Zoo/Zoo/Program.cs
+++ Zoo/Zoo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Zoo.Classes;
+using Zoo.Interfaces;
 
 namespace Zoo
 {
@@ -6,6 +8,20 @@ namespace Zoo
     {
         static void Main(string[] args)
         {
+            // make animals
+            Skunk skunk = new Skunk();
+            Weasel weasel = new Weasel();
+            Bear bear = new Bear();
+            Lion lion = new Lion();
+            Wolf wolf = new Wolf();
+            Narwhal narwhal = new Narwhal();
+            Dolphin dolphin = new Dolphin();
+            Orca orca = new Orca();
+
+            // make dinner
+            IAmDinner rat = new OtherEdibleCritters();
+            IAmDinner mole = new OtherEdibleCritters();
+
 
         }
     }
